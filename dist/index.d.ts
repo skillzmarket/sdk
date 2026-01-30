3e064e58ee6c4ce9e0a71337645c64117f1ca230
Hich.eth <56564585+Hiich@users.noreply.github.com>
2026-01-30 14:15:30 +0100

    Remove dist from git, build on publish

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { SkillzMarket } from './client.js';
-export { DiscoveryClient } from './discovery.js';
-export { createPaymentFetch, createSigner, resolveAccount, getWalletAddress, DEFAULT_NETWORK } from './payment.js';
-export type { Skill, Creator, Review, SearchFilters, SkillzMarketOptions, WalletConfig, } from './types.js';
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { SkillzMarket } from './client.js';
-export { DiscoveryClient } from './discovery.js';
-export { createPaymentFetch, createSigner, resolveAccount, getWalletAddress, DEFAULT_NETWORK } from './payment.js';
-//# sourceMappingURL=index.js.map
